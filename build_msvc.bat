e6ca9c87afda3e5a1b0d7c079f0c6d7ae9c14234
rexim <reximkut@gmail.com>
2025-10-17 19:46:13 +0700

    Try to fix the windows build of SDL2 sowon

--- build_msvc.bat
+++ build_msvc.bat
@@ -6,7 +6,7 @@ set INCLUDES=/I SDL2\include
 set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib Shell32.lib
 
 cl.exe %CXXFLAGS% /Fepng2c png2c.c /link Shell32.lib -SUBSYSTEM:console
-png2c.exe digits.png > digits.h
-png2c.exe penger_walk_sheet.png > penger_walk_sheet.h
+png2c.exe digits.png digits > digits.h
+png2c.exe penger_walk_sheet.png penger > penger_walk_sheet.h
 cl.exe %CXXFLAGS% %INCLUDES% /Fesowon main.c /link %LIBS% -SUBSYSTEM:windows
 cl.exe %CXXFLAGS% /Fesowon_rgfw main_rgfw.c /link Shell32.lib -SUBSYSTEM:windows
